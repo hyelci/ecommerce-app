0b570eb648cf6e3b75c00c61e2123d8ea7998395
Hacer Yelci <haceryelci@gmail.com>
2023-01-19 09:53:48 +0000

    update the search filter

--- src/reducer/ProductsReducer.js
+++ src/reducer/ProductsReducer.js
@@ -2,6 +2,8 @@ import {
   GET_PRODUCTS_BEGIN,
   GET_PRODUCTS_SUCCESS,
   GET_PRODUCTS_ERROR,
+  HANDLE_SEARCH,
+  HANDLE_FILTER,
 } from "../action";
 
 const ProductsReducer = (state, action) => {
@@ -19,11 +21,23 @@ const ProductsReducer = (state, action) => {
       productsLoading: false,
       products: action.payload,
       featuredProducts,
+      visibleProducts: action.payload,
     };
   }
 
   if (action.type === GET_PRODUCTS_ERROR) {
     return { ...state, productsLoading: false, productsError: true };
+  }
+
+  if (action.type === HANDLE_SEARCH) {
+    return { ...state, query: action.payload };
+  }
+
+  if (action.type === HANDLE_FILTER) {
+    const visibleProducts = state.products.filter((product) =>
+      product.name.toLowercase().includes(state.query.toLowercase())
+    );
+    return { ...state, visibleProducts };
   }
 
   throw new Error(`No Matching "${action.type}" - action type`);
